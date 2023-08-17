d101142f445f7b6ac67b332ceb03e45a8a7f2d24
Mattia Bagni <matutebagni@gmail.com>
2023-08-17 18:59:54 -0300

    logger

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -2,6 +2,7 @@ import { Router } from "express"
 import carts_router from './carts.mongo.js'
 import products_router from './products.mongo.js'
 import auth_router from './auth.js'
+import { generateUser, generateProduct } from "../../utils/mocks/generateUserFake.js"
 
 const router = Router()
 
@@ -10,7 +11,7 @@ router.use('/products', products_router)
 router.use('/carts', carts_router)
 router.use('/auth', auth_router)
 
-
+// Endpoint solo para pruebas, /api/mockusers
 router.get('/mockusers', (req,res)=>{
     const { limit } = req.query
 
@@ -37,6 +38,18 @@ router.get('/mockingproducts', (req,res) => {
     return res.status(200).json({
         status:'success',
         payload: products
+    })
+})
+
+router.get("/loggerTest", (req, res) => {
+    req.logger.fatal("Fatal logger test 1")
+    req.logger.error("Error logger test 2")
+    req.logger.warning("Warning logger test 3")
+    req.logger.info("Info logger test 4")
+    req.logger.debug("Debug logger test 5")
+    return res.status(201).json({
+        success: true,
+        message: "logs created!"
     })
 })
 
